2e44da80ee8a57af19a5ba007564d2423f43bbca
Gabe <gabecorso@gmail.com>
2020-10-20 10:03:35 -0700

    update to shwifty

--- finn_image/src/Gallery.js
+++ finn_image/src/Gallery.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Component} from 'react';
 import logo from './logo.svg';
-import {Image, Container, Row, Col} from 'react-bootstrap';
+import {Image, Container, Row, Col, Modal, Spinner} from 'react-bootstrap';
 import './App.css';
 import mosh from './helpers/mosh.js';
 class Gallery extends Component {
@@ -18,15 +18,17 @@ class Gallery extends Component {
     }
     handleClick(image) {
     const {images} = this.state;
+    this.setState({loading: true});
     mosh(
         {
             read: image,
-            mode: 'fatcat',
+            mode: 'schifty',
             headers: {
                 [`Sec-Fetch-Mode`]: `no-cors`
             }
         },
         (err, moshedImg) => {
+            this.setState({loading: false});
             if (err) return console.error(err)
             images.push({value: `data:image/png;base64,${moshedImg.toString('base64')}`, base64: moshedImg.toString('base64')});
             this.setState({images});
@@ -34,19 +36,34 @@ class Gallery extends Component {
     )
     }
     render() {
-        const {images} = this.state;
+        const {images,loading} = this.state;
         return (
         <Container fluid className={'p-5 images-outer-container'}>
+            {!loading &&
             <Row>
                 {images && images.map(image => {
-                    const {value,base64} = image;
+                    const {value, base64} = image;
                     return (
                         <Col sm={6} className={'mb-2'}>
-                            <Image onClick={() => this.handleClick(base64 ? base64 : value)} src={value} fluid/>
+                            <Image onClick={() => this.handleClick(value)} src={value} fluid/>
                         </Col>
                     )
                 })}
             </Row>
+            }
+            {loading &&
+            <Modal
+                show={loading}
+                dialogClassName="modal-90w"
+                aria-labelledby="loading"
+            >
+                <Modal.Body>
+                    <p>
+                       Deconstructing.... stand by.
+                    </p>
+                </Modal.Body>
+            </Modal>
+            }
         </Container>
         );
     }
